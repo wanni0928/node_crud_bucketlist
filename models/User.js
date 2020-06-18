e44d7279ea833da16028da6ae98a48b375fc4b94
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-18 12:31:09 +0900

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    avartarUrl: String,
+    githubId: Number
+});
+
+UserSchema.plugin(passportLocalMongoose, {usernameField: "email"});
+
+const model = mongoose.model("User", UserSchema);
+
+export default model;
