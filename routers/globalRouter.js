16899b1ca05b7fdf58bd8fe98a51ca1d09690391
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-19 14:46:34 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
-import { getJoin, getLogin, postJoin, postLogin, logout } from "../controllers/userController";
+import { getJoin, getLogin, postJoin, postLogin, logout, githubLogin, postGithubLogIn } from "../controllers/userController";
 import { home } from "../controllers/galleryController";
 import { onlyPublic, onlyPrivate } from "../middlewares";
 
@@ -15,6 +16,14 @@ globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 globalRouter.get(routes.logout, onlyPrivate, logout);
+
+//github login
+globalRouter.get(routes.github, githubLogin);
+globalRouter.get(
+    routes.githubCallback, 
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    postGithubLogIn
+);
 
 //home
 globalRouter.get(routes.home, home);
