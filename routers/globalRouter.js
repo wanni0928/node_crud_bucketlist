cd1f58e010ac7ff27cdae48c9b78c03a5911c7ab
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-19 13:38:02 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,18 +2,19 @@ import express from "express";
 import routes from "../routes";
 import { getJoin, getLogin, postJoin, postLogin, logout } from "../controllers/userController";
 import { home } from "../controllers/galleryController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
 
 // join
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
 //login
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
 //home
 globalRouter.get(routes.home, home);
